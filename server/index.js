317b1eaaa5fb9614b7e555d328aa6061cb66a34e
W00ST <matthewortner@gmail.com>
2020-07-25 11:43:00 -0400

    server added

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 8080
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/ping', function (req, res) {
+  return res.send('pong')
+})
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+app.listen(port, () => {console.log(`server started ${port}`)});
+
+
+/** 
+ * create - post
+ * read- get
+ * update- put
+ * delete- delete
+ */

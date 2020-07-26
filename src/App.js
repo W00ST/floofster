b6971e79d3cfd65c49615a22d07563f9c5e4d5b6
W00ST <matthewortner@gmail.com>
2020-07-25 21:09:22 -0400

    basic layout of landing and login. add logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Pug.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="loginBorder"> FLOOFSTER
+        <img src={logo} alt="Logo" />
+        <div className="newEmail">
+          <div>name</div>
+          <div>username/owner's name</div>
+          <div>email</div>
+          <div>handle google login</div>
+        </div>
+      <input type="button" className="join" value="Join" />
+      </div>
     </div>
   );
 }

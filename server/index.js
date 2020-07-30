cb55cbecdb7197aad871ddd509149cca6dbbef4d
W00ST <matthewortner@gmail.com>
2020-07-30 14:50:24 -0400

    server serves login from views, login info reaches db, client re-directed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const pool = require('./database')
 
 const login = require('./routes/login')
 
@@ -8,7 +9,11 @@ const app = express()
 const port = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'styles')))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.get('/login', login)
 
@@ -20,8 +25,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
+app.post('/register', function (req, res) {
+  const params = [req.body.name, req.body.email, req.body.password]
+  pool.query('INSERT INTO owners(name,email,password) VALUES($1, $2, $3);', params, (err) => {
+    if (err) return res.send(err);
+    return res.redirect('/')
+  })
+})
 app.listen(port, () => {console.log(`${new Date()} server started ${port}`)});
+
 
 
 /** 
